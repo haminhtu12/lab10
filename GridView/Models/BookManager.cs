9b0ffa653973b81f8295573404dc3ff4083b5dc5
Tu Ha <54472431+haminhtu12@users.noreply.github.com>
2020-08-31 11:24:06 +0700

    first commit

---
+++ GridView/Models/BookManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GridView.Models
+{
+    public class BookManager
+    {
+        public static List<Book> GetBooks()
+        {
+            var books = new List<Book>();
+            books.Add(new Book { BookId = 1, Title = "Naruto", Author = "Uzumaki", CoverImage = "Assets/1.jpg" });
+            books.Add(new Book { BookId = 2, Title = "Itachi ", Author = "Uchiha", CoverImage = "Assets/2.jpg" });
+            books.Add(new Book { BookId = 3, Title = "Kakashi ", Author = "",  CoverImage = "Assets/3.jpg" });
+            return books;
+        }
+
+    }
+}
